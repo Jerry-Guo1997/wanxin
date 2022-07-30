27d72dfe4d25e68c649b5d27933e1297aba99886
jerry <1104955854@qq.com>
2022-07-30 16:59:56 +0800

    day 2 :添加account service

---
+++ wanxinp2p-account-service/src/main/java/cn/itcast/wanxinp2p/account/service/AccountService.java
@@ -0,0 +1,7 @@
+package cn.itcast.wanxinp2p.account.service;
+
+import cn.itcast.wanxinp2p.common.domain.RestResponse;
+
+public interface AccountService {
+    RestResponse getSMSCode(String mobile) ;
+}
